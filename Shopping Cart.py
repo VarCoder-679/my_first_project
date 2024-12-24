ab9d86d8ced71e75c32ef274274e4e0d4d451c67
Shashwat <shashwat4445@gmail.com>
2024-12-24 13:59:15 +0530

    Shopping Cart.py
    food cart contains all food items and the total price of all the items

---
+++ Shopping Cart.py
@@ -0,0 +1,23 @@
+##--->SHOPPING CART IN PYTHON
+foods = []
+prices =[]
+total=0
+
+name=input("Enter the name of the customer: ")
+while True:
+    food = input("Enter the food(q or Q for quit): ")
+    if food.lower()=="q" and "Q":
+        break
+    else:
+        price = float(input(f"Enter the price of {food}: Rs. "))
+        foods.append(food)
+        prices.append(price)
+
+print(f" ------{name}'s FOOD CART------- ")
+
+for i in range(len(foods)):
+    print(f"{foods[i]} : Rs. {prices[i]}")
+
+for price in prices:
+    total += price
+print(f"Your total is: Rs.{total}")
